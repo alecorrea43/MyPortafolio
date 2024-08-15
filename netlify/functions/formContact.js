1362abd4a463cdcfecb3a84617eaa731dd5ddfc4
Alejandro <itznecro.4376761@gmail.com>
2024-08-15 18:18:20 -0300

    a

--- netlify/functions/formContact.js
+++ netlify/functions/formContact.js
@@ -13,9 +13,9 @@ exports.handler = async function(event, context) {
 
   // Configuración del cliente OAuth2
   const oAuth2Client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URI
+    process.env.GMAIL_CLIENT_ID,
+    process.env.GMAIL_CLIENT_SECRET,
+    process.env.GMAIL_REDIRECT_URI
   );
 
   oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
